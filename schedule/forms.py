32d354776a783b2086eaa43e2afb233a186c77e1
Joseph Shieh <josephs2@uw.edu>
2012-10-17 17:14:20 -0700

    Took out the timeperiod and location part because it is already being updated in the views.

--- schedule/forms.py
+++ schedule/forms.py
@@ -3,22 +3,24 @@ from labgeeksrpg.people.models import UserProfile
 from labgeeksrpg.schedule.models import TimePeriod
 from labgeeksrpg.chronos.models import Location
 
+
 class SelectTimePeriodForm(forms.ModelForm):
 
     working_periods = forms.ModelMultipleChoiceField(
         required=False,
         widget=forms.CheckboxSelectMultiple,
         queryset=TimePeriod.objects.all().order_by('start_date'),
-        help_text='Please select the time periods in which you are available for work.' 
+        help_text='Please select the time periods in which you are available for work.'
     )
 
-    def save(self,*args,**kwargs):
-        inst = forms.ModelForm.save(self,*args,**kwargs)
+    def save(self, *args, **kwargs):
+        inst = forms.ModelForm.save(self, *args, **kwargs)
         return inst
 
     class Meta:
         model = UserProfile
         fields = ('working_periods',)
+
 
 class CreateDailyScheduleForm(forms.Form):
     DAY_CHOICES = (
@@ -30,10 +32,10 @@ class CreateDailyScheduleForm(forms.Form):
         ('Friday', 'Friday'),
         ('Saturday', 'Saturday'),
     )
-    timeperiods = forms.ChoiceField(choices=[(tp.name,tp.name) for tp in TimePeriod.objects.all()])
-    location = forms.ChoiceField(choices=[(loc.name,loc.name) for loc in Location.objects.all()],required=True)
+    timeperiods = forms.ChoiceField(choices=[])
+    location = forms.ChoiceField(choices=[], required=True)
 
 
 class SelectDailyScheduleForm(forms.Form):
-    location = forms.ChoiceField(widget=forms.RadioSelect,choices=[(loc.name,loc.name) for loc in Location.objects.all()],required=True)
+    location = forms.ChoiceField(widget=forms.RadioSelect, choices=[(loc.name, loc.name) for loc in Location.objects.all()], required=True)
     day = forms.DateField()
