0600c72e125bdc7f4c7d75d84c10b319e318ee62
Jon Swanson <jswan2@uw.edu>
2012-12-18 16:24:47 -0800

    fixed packages in setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name = 'labgeeks-horae'
+    version = '1.0',
+    license = 'Apache',
+    url = 'http://github.com/abztrakt/labgeeks_horae',
+    description = 'The schedule app for the labgeeks suite of student staff management tools.',
+    author = 'Craig Stimmel',
+    packages = ['labgeeks_horae',],
+    install_requires = [
+        'setuptools',
+        'South==0.7.3',
+        'labgeeks-people',
+        'labgeeks-chronos',
+    ],
+)
