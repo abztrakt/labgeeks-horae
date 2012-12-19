2d91502de42cd6fde2b5c26614a8a84f368f6a8e
Jon Swanson <jswan2@uw.edu>
2012-12-18 16:33:30 -0800

    fixed syntax error in setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name = 'labgeeks-horae'
+    name = 'labgeeks-horae',
     version = '1.0',
     license = 'Apache',
     url = 'http://github.com/abztrakt/labgeeks_horae',
